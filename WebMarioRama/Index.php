ef1c5e7fb9bda0db706e5f18726dcb8d22cc1677
Devaud <devaud.alan@gmail.com>
2014-11-14 14:44:47 +0100

    Ajout de certin document.
    Création de la page index + ajout de bootstrap.

---
+++ WebMarioRama/Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="./css/bootstrap-theme.min.css">
+
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="./js/bootstrap.min.js"></script>
+
+        <title>WebMarioRama</title>
+    </head>
+    <body>
+
+    </body>
+</html>
