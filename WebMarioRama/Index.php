a8e628a9b2c1441e660b95b59c54197e52e031f7
Devaud <devaud.alan@gmail.com>
2014-11-14 15:49:34 +0100

    Création de la maquette

--- WebMarioRama/Index.php
+++ WebMarioRama/Index.php
@@ -7,18 +7,71 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <!-- Latest compiled and minified CSS -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/Normalize.css"
+        <!-- Fichier pour bootstrap -->
         <link rel="stylesheet" href="./css/bootstrap.min.css">
-
-        <!-- Optional theme -->
         <link rel="stylesheet" href="./css/bootstrap-theme.min.css">
-
-        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="./js/bootstrap.min.js"></script>
+
+        <!-- Fichier créer par nous -->
+        <link rel="stylesheet" href="./css/style.css">
 
         <title>WebMarioRama</title>
     </head>
     <body>
-
+        <div id="page">
+            <!-- Nav -->
+            <nav class="navbar navbar-default">
+                <section class="container-fluid">
+                    <section class="navbar-header">
+                        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="./Index.php">WebMarioRama</a>
+                    </section>
+                    <section id="navbarCollapse" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="./Index.php">Home</a></li>
+                            <li><a href="#">Jeux</a></li>
+                            <li><a href="#">Consoles</a></li> 
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        </ul>
+                    </section>
+                </section>
+            </nav>
+            
+            <!-- Contenu -->
+            <section class="container-fluid">
+                <section class="col-sm-12">
+                    <section class="panel panel-default">
+                        <p>bdsmkélvmdslékymvsadvasdvasv
+                        sav
+                        sa
+                        vsavsa</p>
+                        <p>vsav
+                        sadv
+                        as</p>
+                        <p> vsa
+                        vvs
+                        csacsa</p>
+                    </section>
+                </section>
+            </section>
+            
+            <!-- pied de page -->
+            <footer class="col-sm-12">
+                <section class="panel panel-footer">
+                    <p>CFPT-I Examen M152 2014-2015 Devaud Alan & Geinoz Mathieu</p>
+                </section>
+            </footer>
+        </div>
     </body>
 </html>
